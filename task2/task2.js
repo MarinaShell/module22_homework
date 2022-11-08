3158f842e70e0b11e4be1cc234488892fe545117
Ponka Petrova <marinashell@mail.ru>
2022-11-08 07:56:35 +0300

    edit task2

--- task2/task2.js
+++ task2/task2.js
@@ -3,20 +3,14 @@ let icon1 = document.querySelector('.btn_icon1');
 let icon2 = document.querySelector('.btn_icon2');
 
 btn.addEventListener('click', () => {
-
-  console.log(icon2);
   if (icon2.style.display == 'none')
-  {
-    console.log("1"+icon2.style.display);
+  {    
     icon1.style.display = 'none';
     icon2.style.display = 'block';
    }  
   else
-  {
-    console.log("2"+icon2.style.display);
+  {    
     icon2.style.display = 'none';
     icon1.style.display = 'block';
   }  
-
-
 });
